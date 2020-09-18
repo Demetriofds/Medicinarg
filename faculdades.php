c40b0db9502c0e6de0b21ca3333f222b89d1bcd6
Demetrio Silva <demetriofds@gmail.com>
2020-09-18 11:28:20 -0300

    inclusao do menu - include
    coloquei o menu no include

--- faculdades.php
+++ faculdades.php
@@ -84,37 +84,8 @@
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<!-- Menu das paginas -->
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul class="navbar-nav ml-lg-auto text-center">
-							<li class="nav-item active  mr-lg-3">
-								<a class="nav-link" href="index.php">Home
-									<span class="sr-only">(current)</span>
-								</a>
-							</li>
-							<li class="nav-item mr-lg-3">
-								<a class="nav-link" href="quem-somos.php">Quem Somos</a>
-							</li>
-							<li class="nav-item mr-lg-3">
-								<a class="nav-link" href="faculdades.php">Faculdades</a>
-							</li>
-
-							<li class="nav-item mr-3">
-								<a class="nav-link" href="contato.php">contato</a>
-							</li>
-							<!-- search --->
-							<div class="search-bar-agileits">
-								<div class="cd-main-header">
-									<ul class="cd-header-buttons">
-										<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
-									</ul>
-									<!-- cd-header-buttons -->
-								</div>
-								<!-- botao de pesquisa -->
-								<?php include_once ('include/pesquisa-banner-top.php'); ?>
-							</div>
-							<!-- search --->
-						</ul>
-					</div>
+				<?php include_once ('include/menu.php'); ?>
+				
 				<!-- //main nav -->
 			</nav>
 			<!-- //nav -->
