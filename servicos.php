34d6f7df358185ba4ca0823df7a41e9e96f3dde3
Demetrio Silva <demetriofds@gmail.com>
2020-09-18 11:40:26 -0300

    Inclusão do menu include
    coloquei o menu na pasta include

--- servicos.php
+++ servicos.php
@@ -87,53 +87,7 @@
 				<span class="navbar-toggler-icon"></span>
 			</button> 
 			<!-- Menu das paginas -->
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav ml-lg-auto text-center">
-					<li class="nav-item active  mr-lg-3">
-						<a class="nav-link" href="index.php">Home
-							<span class="sr-only">(current)</span>
-						</a>
-					</li>
-					<li class="nav-item mr-lg-3">
-						<a class="nav-link" href="quem-somos.php">Quem Somos</a>
-					</li>
-					<li class="nav-item mr-lg-3">
-						<a class="nav-link" href="servicos.php">Serviços</a>
-					</li>
-					<li class="dropdown nav-item mr-lg-3">
-						<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle ">
-							Paginas
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu" role="menu">
-							<li class="nav-item">
-								<a href="faculdades.php" class="nav-link">Faculdades</a>
-							</li>
-							<li class="nav-item">
-								<a href="parceiros/cadastro_parceiros.php" class="nav-link">Cadastro Parceiros</a>
-							</li>
-							<!--<li class="nav-item">
-								<a href="error.html" class="nav-link">404</a>
-							</li> -->
-						</ul>
-					</li>
-					<li class="nav-item mr-3">
-						<a class="nav-link" href="contato.php">contato</a>
-					</li>
-					<!-- search --->
-					<div class="search-bar-agileits">
-						<div class="cd-main-header">
-							<ul class="cd-header-buttons">
-								<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
-							</ul>
-							<!-- cd-header-buttons -->
-						</div>
-						<!-- botao de pesquisa -->
-								<?php include_once ('include/pesquisa-banner-top.php'); ?>
-					</div>
-					<!-- search --->
-				</ul>
-			</div>
+			<?php include_once ('include/menu.php'); ?>
 			<!-- //main nav -->
 		</nav>
 		<!-- //nav -->
